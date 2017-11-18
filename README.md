67a4fea63ac7a7d42b7f9134fc6ac3ccbfaa7217
bw <thatwasawkward@gmail.com>
2017-11-18 13:36:36 -0800

    Adding additional methods and updating the readme.

--- README.md
+++ README.md
@@ -3,13 +3,35 @@ Extremely simple module for getting a random value from an array. Completely ind
 EXAMPLE:
 
 ```javascript
+const whate = require('whate')
+
 const array = [
-	"one",
-	"two",
-	"three"
+	'one',
+	2,
+	{'three': 3}
 ]
 
 const randomValue = whate.ver(array)
 
-console.log(randomValue) // "one", "two", or "three"
+console.log(randomValue) // **'one'**, **2**, or **{'three': 3}**
 ```
+
+# Methods
+
+### .ver(array)
+Returns a random value of any type.
+
+### .str(array)
+Returns a random string.
+
+### .num(array)
+Returns a random number.
+
+### .int(array)
+Returns a random integer.
+
+### .arr(array)
+Returns a random array.
+
+### .obj(array)
+Returns a random non-array object.
--- whate.js
+++ whate.js
@@ -1,3 +1,28 @@
 exports.ver = (x) => {
-	return x[Math.floor(Math.random() * x.length)];
+	return x[Math.floor(Math.random() * x.length)]
+}
+
+exports.str = (x) => {
+	const strs = x.filter((s) => typeof s == 'string')
+	return exports.ver(strs)
+}
+
+exports.num = (x) => {
+	const nums = x.filter((n) => typeof n == 'number')
+	return exports.ver(nums)
+}
+
+exports.int = (x) => {
+	const ints = x.filter((i) => Number.isInteger(i) == true)
+	return exports.ver(ints)
+}
+
+exports.arr = (x) => {
+	const arrs = x.filter((a) => Array.isArray(a) == true)
+	return exports.ver(arrs)
+}
+
+exports.obj = (x) => {
+	const objs = x.filter((o) => typeof o == 'object' && o !== null && Array.isArray(o) == false)
+	return exports.ver(objs)
 }
